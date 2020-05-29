c4d93b7e10888d08b90f2f9b8f907c564d219af0
mezzlasha <mezzlasha@gmail.com>
2020-05-29 12:46:21 +0000

    Added Lockscreen Fod Overlap Fix and some strings to enable features like smart pixels and notch hiding

--- device.mk
+++ device.mk
@@ -94,7 +94,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay\
+    $(LOCAL_PATH)/overlay-aosp
 
 # Properties
 -include $(LOCAL_PATH)/system_prop.mk
